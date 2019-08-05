b1f29e8f27968f12ab4aec66c96ccf60036afd27
dtmkeng <kang081@hotmail.com>
2019-08-05 22:28:35 +0700

    add buffalo go

---
+++ buffalo/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/envy"
+	contenttype "github.com/gobuffalo/mw-contenttype"
+	paramlogger "github.com/gobuffalo/mw-paramlogger"
+	"github.com/gobuffalo/x/sessions"
+	"github.com/rs/cors"
+)
+
+var r *render.Engine
+
+// ENV ...
+var ENV = envy.Get("GO_ENV", "development")
+
+func main() {
+
+	app := buffalo.New(buffalo.Options{
+		Env:          ENV,
+		SessionStore: sessions.Null{},
+		PreWares: []buffalo.PreWare{
+			cors.Default().Handler,
+		},
+		SessionName: "_coke_session",
+		Addr:        ":8080",
+	})
+	app.Use(paramlogger.ParameterLogger)
+	app.Use(contenttype.Set("application/json"))
+
+	app.GET("/", HomeHandler)
+	// app.Options.Addr := 8080
+	app.Serve()
+
+}
+
+// HomeHandler is a default handler to serve up
+// a home page.
+func HomeHandler(c buffalo.Context) error {
+	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
+}
