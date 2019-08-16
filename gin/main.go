55a9a118f60669c15f540563d0503987be191710
dtmkeng <kang081@hotmail.com>
2019-08-16 22:14:01 +0700

    update query gin and beego

--- gin/main.go
+++ gin/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -8,6 +12,12 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
